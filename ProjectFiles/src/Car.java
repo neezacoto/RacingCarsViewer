97c5ec6e44435a5ea2e73f3ea0bcc78b7dd97566
davids251 <99621985+davids251@users.noreply.github.com>
2022-03-23 12:13:33 -0400

    test branch update
    test

--- ProjectFiles/src/Car.java
+++ ProjectFiles/src/Car.java
@@ -1,84 +1,80 @@
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import java.util.Random;
+import java.util.ArrayList;
 import javafx.animation.PathTransition;
-import javafx.scene.shape.HLineTo;
-import javafx.scene.shape.MoveTo;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Path;
-import javafx.scene.shape.Polyline;
+import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
+import javafx.scene.shape.Polyline;
 /**
- * This class is responsible for rendering the car and 
+ * This class is responsible for rendering the car and
  * making it race down a path reporting when it finishes
  * -
- * Christian Rudder: Made the skeleton connecting it to demonstrate how 
+ * Christian Rudder: Made the skeleton connecting it to demonstrate how
  * a car object will show on the racetrack and how it will report it's win. Also including
  * an initial override for toString and Equals
  */
+
+
 public class Car extends Rectangle{
     private String color;
     private int speed;
-    private int[] track; //coordinate path that the car has to follow
-
-    private Random rand = new Random(); //random is used to randomize the value of the car
+     private ArrayList<Integer> track; //coordinate path that the car has to follow
+    PathTransition transition;
+    
 
     /**
      * The Car constructor
      */
+
     Car() {
         super(50,30,Color.GREEN);
         this.color = "Green";
+        
+        // Circle circle = new Circle(100);
+
+        Polyline polyline = new Polyline();
+        polyline.getPoints().addAll(new Double[] {
+            -100.0, -100.0,
+            100.0, -100.0,
+            100.0, 150.0,
+            -100.0,150.0,
+            -100.0,-100.0
+
+ 
+        });
+
+        transition = new PathTransition();
+        transition.setNode(this);
+        transition.setDuration(Duration.seconds(3));
+        transition.setPath(polyline);
+        transition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        transition.setCycleCount(PathTransition.INDEFINITE);
+        
         //finishRaceReport();
-       
-        
-        Path path = new Path();
-        path.getElements().add(new MoveTo(0,0));
-        path.getElements().add(new HLineTo(200));
-        PathTransition move = new PathTransition();
-        move.setNode(this);
-        move.setDelay(Duration.seconds(2));
-        move.setPath(path);
-        move.play();
-        
-        
     }
-
-    Car(int carNumber){ //Used to give the cars specific paths according to their numbers
-        super(50,30,Color.GREEN);
-        this.color = "Green";
-        speed = 0;
-        randomizeSpeed();
-
-        if(carNumber == 1){
-            track = new int[]{1, 2, 3, 4};
-        } else if (carNumber == 2){
-            track = new int[]{2, 3, 4, 1};
-        } else if (carNumber == 3){
-            track = new int[]{3, 4, 1, 2};
-        } else if (carNumber == 4){
-            track = new int[]{4, 1, 2, 3};
-        } else {
-            track = new int[]{0, 0, 0, 0};
-        }
-
-
+        Car(int speed, Color carColor, ArrayList<Integer> track){ //Used to give the cars specific paths according to their numbers
+        super(50,30, carColor);
+        this.speed = speed;
+        this.track = track;
     }
+    
+    
     /**
      * starts following path when told to from RaceTrack startRace()
      */
     public void startEngine() {
-
+        transition.play();
     }
 
-
-    /**
+      /**
      * getter for the race track's track
      * @return race track coordinates 
      */
-    public int[] getTrack() {
+    public ArrayList<Integer> getTrack() {
         return this.track;
     }
-    
+
     /**
      * getter for the car color
      * @return car color
@@ -94,8 +90,8 @@ public class Car extends Rectangle{
     public int getSpeed() {
         return this.speed;
     }
-
-    /**
+    
+     /**
      * responsible for displaying the win alert once this car has won
      */
     private void finishRaceReport() {
@@ -126,27 +122,19 @@ public class Car extends Rectangle{
 
         return this.color.equals(car.getColor()) && this.speed == car.getSpeed() && this.track == car.getTrack();
     }
-
-    /*
-    Method to randomize the speed of the car again if any cars have the matching speed -JL
-    */
-
-    public void randomizeSpeed(){
-        while(speed == 0) {
-            speed = rand.nextInt(5);
-        }
-    }
-
-
-    //method used during development to check parameters of the car -JL
+        //method used during development to check parameters of the car -JL
     public void carPrinterTester(){
-        System.out.println("Car's path is " + track[0] + " " + track[1] + " " +track[2] + " " + track[3] + " \n with speed: " + speed);
+        System.out.print("Car's path is: ");
+        for(int i = 0; i < 4; i++){
+            System.out.print(track.get(i) + " ");
+        }
+        System.out.println("\n With Speed: " + speed);
 
     }
+    
 
 
 
 
- 
 
 }
--- ProjectFiles/src/RaceTrack.java
+++ ProjectFiles/src/RaceTrack.java
@@ -2,14 +2,17 @@
  * @author 
  */
 import java.util.ArrayList;
-import javafx.animation.PathTransition;
-
+import java.util.Collections;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.StackPane;
-import javafx.scene.shape.*;
-import javafx.util.Duration;
+import javafx.scene.paint.Color;
+
+/**
+ * to comminicate with scenecrontroller
+ * SceneController.getInstance().methodName()
+ */
 
 /**
  * This class will be responsible for creating the race track that holds all
@@ -19,14 +22,11 @@ import javafx.util.Duration;
  * This in the begininng allowed for a car object to move to a position and trigger the win alert upon race start.
  * Also including an initial Override to the toString method
  */
+
 public class RaceTrack extends StackPane { 
     private static RaceTrack instance;//the instance of the RaceTrack
     private int[] track;//the checkpoint path coordinates
     private ArrayList<Car> cars;//all the cars in play
-    private Car carz = new Car();
-    private ArrayList<Integer> checkPoints;
-    private int laps;
-    private ArrayList<Car> car;
 
     /**
 	 *Creating an instance of the RaceTrack to be referenced
@@ -35,7 +35,6 @@ public class RaceTrack extends StackPane {
 	 */
     public static RaceTrack newInstance() {
         //this is where it's calling the Constructor
-        instance = new RaceTrack(new Car());
         return instance;
     }
 
@@ -53,17 +52,66 @@ public class RaceTrack extends StackPane {
      * @author Christian Rudder
      */
 
-    public RaceTrack(Car car) {        
-        cars = new ArrayList();
+     public RaceTrack() {
+        //Old code used to start the initialization of the racetrack
+        /*cars = new ArrayList();
         this.cars.add(car);
         this.setStyle("-fx-background-color: black");
-        
-        
-        
         Button button = new Button();
         
-        this.getChildren().addAll(car);
+        this.getChildren().addAll(car);*/
+
+
+        cars = new ArrayList<>();
+
+        ArrayList<Color> carColors = new ArrayList<Color>();
+        carColors.add(Color.GREEN);
+        carColors.add(Color.RED);
+        carColors.add(Color.CYAN);
+        carColors.add(Color.YELLOW);
+
+        ArrayList<Integer> track = new ArrayList<>();
+        for(int j = 1; j < 5; j++){
+            track.add(j);
+        }
+
+        ArrayList<Integer> carSpeeds = generateRandomSpeedList();
+
+        ArrayList<Integer> carPath = new ArrayList<Integer>();
+        for(int i = 1; i < 5; i++){
+            carPath.add(i);
+        }
+
+        for(int i = 0; i < 4; i++){
+            cars.add(new Car(carSpeeds.get(i), carColors.get(i), carPath));
+            int temp = carPath.get(0);
+            carPath.remove(0);
+            carPath.add(temp);
+        }
+
+
+
+        //Loop used to test to see what information the cars have
+        /*for(int i = 0; i < 4; i++){
+            cars.get(i).carPrinterTester();
+        }*/
+
     }
+     
+     
+    //used to generate speeds from an integer to another then randomizes the order of the speeds -JL
+    public ArrayList<Integer> generateRandomSpeedList(){
+        ArrayList<Integer> speeds = new ArrayList<Integer>();
+
+        for(int i = 5; i < 10; i++){
+            speeds.add(i);
+        }
+
+        Collections.shuffle(speeds);
+        System.out.println(speeds);
+        return speeds;
+    }
+
 
     /**
      * getter for the RaceTrack UI
@@ -80,23 +128,17 @@ public class RaceTrack extends StackPane {
     public void startRace() {
         //this is just some test code to check if the start button is being called
         //feel free to comment out or delete once you start writing your logic
-        Alert alert = new Alert(AlertType.CONFIRMATION);
-		alert.setTitle("Yep");
-		alert.setHeaderText("start race works");
-		alert.setContentText("press ok to do nothing");
+        // Alert alert = new Alert(AlertType.CONFIRMATION);
+		// alert.setTitle("Yep");
+		// alert.setHeaderText("start race works");
+		// alert.setContentText("press ok to do nothing");
 
-		alert.showAndWait();
+		// alert.showAndWait();
         //------------------------------------------------------------------------
-        
+        for (Car car : cars) {
+            car.startEngine();
+        }
     }
-    
-    public ArrayList getCheckPoint(){
-        return checkPoints;
-    }
-    
-    
-    
-    
 
     /**
      * Getter for the track coordinates
@@ -127,6 +169,5 @@ public class RaceTrack extends StackPane {
 
         return "Track Checkpoints: " + this.track + "\n" + carList;
     }
-
 
 }
